90d2b406e9adc221b5940c5ab1d0544429a7bfdf
Anshika Chamoli <119307723+anshikachamoli18@users.noreply.github.com>
2024-07-27 22:17:29 +0530

    index.js model/ connected to database

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import {connectDB} from './database/db.js';
+import cors from 'cors';
+
+dotenv.config();
+
+
+const app=express();
+app.use(express.json());
+app.use(cors());
+
+const port=process.env.PORT ;
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+})
+
+/*app.use("/uploads",express.static('uploads'));
+
+import userRoutes from './routes/user.js';
+import courseRoutes from './routes/course.js';
+import adminRoutes from './routes/admin.js';
+
+
+app.use('/api',userRoutes);
+app.use('/api',courseRoutes);
+app.use('/api',adminRoutes);*/
+
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+    connectDB();
+})
